9c49c41634b608e0ce79e5099402bda4ef1ba5e3
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-14 20:49:25 +0100

    More explicit name We need to run a job for each server. Google does not allow different vars to be specified on the trigger

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcloud config set project fnic-2024
-gcloud run jobs deploy modreportbot --source . --set-env-vars=LEMMY_USER="fnicmodbot",LEMMY_INSTANCE="lemmings.world",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!TcCyUEXBiAZZOviESk:matrix.org" --set-secrets="LEMMY_PW=fnicmodbot-lemmings-world:latest","MATRIX_PW=fnicreports:latest"
+gcloud run jobs deploy modreportbot-lemmings-world --source . --set-env-vars=LEMMY_USER="fnicmodbot",LEMMY_INSTANCE="lemmings.world",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!TcCyUEXBiAZZOviESk:matrix.org" --set-secrets="LEMMY_PW=fnicmodbot-lemmings-world:latest","MATRIX_PW=fnicreports:latest"
