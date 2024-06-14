22d0961de989d22b23e022fba1c691897ca80ea5
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-14 21:50:41 +0100

    update for me

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud config set project fnic-2024
-gcloud run jobs deploy modreportbot-lemmings-world --source . --set-env-vars=LEMMY_USER="fnicmodbot",LEMMY_INSTANCE="lemmings.world",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!TcCyUEXBiAZZOviESk:matrix.org" --set-secrets="LEMMY_PW=fnicmodbot-lemmings-world:latest","MATRIX_PW=fnicreports:latest"
+gcloud config set project bots-424808
+gcloud run jobs deploy modreportbot --source . --set-env-vars=LEMMY_USER="modreportbot",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com" --set-secrets="LEMMY_PW=modreportbot:latest","MATRIX_PW=robotbunny:latest"
