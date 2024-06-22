01f0e22e340d01dbbe3f918c1c27f8c0f7fe4d71
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-22 22:02:59 +0100

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud run jobs deploy modreportbot --region europe-west1 -- project bots-424808 --source . --set-env-vars=LEMMY_USER="eric",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com" --set-secrets="LEMMY_PW=eric:latest","MATRIX_PW=robotbunny:latest"
+gcloud run jobs deploy modreportbot --region europe-west1 --project bots-424808 --source . --set-env-vars=LEMMY_USER="eric",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com" --set-secrets="LEMMY_PW=eric:latest","MATRIX_PW=robotbunny:latest"
