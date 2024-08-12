c5f546605b26903685ac941bfd2833f3b018ea53
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-08-12 12:48:48 +0100

    remove set project

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ gc_deploy() {
   gcloud run jobs deploy modreportbot-$1 --project=$project --region=$region --source . --set-env-vars=LEMMY_USER="fnicmodbot",LEMMY_INSTANCE="$2",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="$matrix_room",FLAG_DOWNVOTES="$flag_dv",GET_MESSAGES="$flag_pm" --set-secrets="LEMMY_PW=fnicmodbot-$1:latest","MATRIX_PW=fnicreports:latest" &
 }
 
-gcloud config set project fnic-2024
-
 gc_deploy "lemmings-world" "lemmings.world"
 gc_deploy "dbzer0" "lemmy.dbzer0.com"
 gc_deploy "lemm-ee" "lemm.ee"
