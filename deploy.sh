d012ff38daf8fc13150af64865baa8f68aceb7ae
Chris Young <chris@unsatisfactorysoftware.co.uk>
2025-01-20 14:42:23 +0000

    Create second bot for new post notifications ... to avoid conflicts with sightengine, which marks posts as read when flagged

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcloud run jobs deploy modreportbot --region europe-west1 --project bots-424808 --source . --set-env-vars=LEMMY_USER="eric",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com",FLAG_DOWNVOTES="FALSE",GET_MESSAGES="TRUE",NOTIFY_NEW="TRUE" --set-secrets="LEMMY_PW=eric:latest","MATRIX_PW=robotbunny:latest"
+gcloud run jobs deploy modreportbot --region europe-west1 --project bots-424808 --source . --set-env-vars=LEMMY_USER="eric",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com",FLAG_DOWNVOTES="FALSE",GET_MESSAGES="TRUE",NOTIFY_NEW="FALSE" --set-secrets="LEMMY_PW=eric:latest","MATRIX_PW=robotbunny:latest" &
+
+gcloud run jobs deploy modreportbot-notify --region europe-west1 --project bots-424808 --source . --set-env-vars=LEMMY_USER="charles_petrescu",LEMMY_INSTANCE="feddit.uk",MATRIX_USER="@robot_bunny:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="!JvATVaovRwoZeJNnas:beeper.com",FLAG_DOWNVOTES="FALSE",GET_MESSAGES="TRUE",NOTIFY_NEW="TRUE" --set-secrets="LEMMY_PW=charles:latest","MATRIX_PW=robotbunny:latest" &
+
+wait
+
