8782b70270043104f5bb42773b0aa86f19f83d90
Chris Young <chris@unsatisfactorysoftware.co.uk>
2025-01-29 13:46:29 +0000

    Tidy removed_posts into own function

--- modlog.py
+++ modlog.py
@@ -6,6 +6,44 @@ import firestore
 from pythorhead import Lemmy
 from pythorhead.types import ModlogActionType,ListingType
 
+def removed_posts(lemmy, live, c, available_communities, room, muser, mpw, mserver):
+  ml = lemmy.modlog.get(community_id=c,type_=ModlogActionType.ModRemovePost,limit=4)
+  processed = []
+  ppid = 0
+
+  for log in ml['removed_posts']:
+    if ppid == log['mod_remove_post']['post_id']:
+      continue # skip duplicates
+    ppid = log['mod_remove_post']['post_id']
+
+    if log['mod_remove_post']['id'] in processed_modlogs['removed_posts']:
+      break # stop processing if we've already seen a log as they are in descending order
+
+    if "reason" in log['mod_remove_post']:
+      reason = log['mod_remove_post']['reason']
+    else:
+      reason = "None given"
+    msg = f"\"{log['post']['name']}\" in \"{log['community']['name']}\" has been removed due to reason: {reason}"
+    print(f"{log['mod_remove_post']['id']} {msg}")
+    user=lemmy.user.get(person_id=log['post']['creator_id']) # look up user
+    if 'display_name' in user['person_view']['person']:
+      msg_to = user['person_view']['person']['display_name']
+    else:
+      msg_to = user['person_view']['person']['name']
+
+    if live:
+      processed.append(log['mod_remove_post']['id']) # mark modlog as processed
+      if c in available_communities['removed_posts']: # only perform actions if we've seen the community before
+        # Post to Matrix
+        matrix.post(f'[modlog] {msg}', room, muser, mpw, mserver)
+        # Send PM to the poster
+        if pm_modlogs is True:
+          pm_msg = f"Dear {msg_to},\n\nYour post {msg}\n\nIf you are able to correct this please feel free to re-post."
+          lemmy.private_message.create(recipient_id=log['post']['creator_id'],content=pm_msg)
+
+  return(processed)
+
+
 def run(lemmy, l_user, l_inst, live, room, muser, mpw, mserver, pm_modlogs):
   processed_modlogs = {}
   processed_modlogs['removed_posts'] = []
@@ -40,40 +78,10 @@ def run(lemmy, l_user, l_inst, live, room, muser, mpw, mserver, pm_modlogs):
 
   # Get the modlog for each community
   for c in commlist:
-    ml = lemmy.modlog.get(community_id=c,type_=ModlogActionType.ModRemovePost,limit=4)
+    processed = removed_posts(lemmy, live, c, available_communities, room, muser, mpw, mserver)
 
-    ppid = 0
-
-    for log in ml['removed_posts']:
-      if ppid == log['mod_remove_post']['post_id']:
-        continue # skip duplicates
-      ppid = log['mod_remove_post']['post_id']
-
-      if log['mod_remove_post']['id'] in processed_modlogs['removed_posts']:
-        break # stop processing if we've already seen a log as they are in descending order
-
-      if "reason" in log['mod_remove_post']:
-        reason = log['mod_remove_post']['reason']
-      else:
-        reason = "None given"
-      msg = f"\"{log['post']['name']}\" in \"{log['community']['name']}\" has been removed due to reason: {reason}"
-      print(f"{log['mod_remove_post']['id']} {msg}")
-      user=lemmy.user.get(person_id=log['post']['creator_id']) # look up user
-      if 'display_name' in user['person_view']['person']:
-        msg_to = user['person_view']['person']['display_name']
-      else:
-        msg_to = user['person_view']['person']['name']
-
-      if live:
-        processed_modlogs['removed_posts'].append(log['mod_remove_post']['id']) # mark modlog as processed
-        if c in available_communities['removed_posts']: # only perform actions if we've seen the community before
-          # Post to Matrix
-          matrix.post(f'[modlog] {msg}', room, muser, mpw, mserver)
-          # Send PM to the poster
-          if pm_modlogs is True:
-            pm_msg = f"Dear {msg_to},\n\nYour post {msg}\n\nIf you are able to correct this please feel free to re-post."
-            lemmy.private_message.create(recipient_id=log['post']['creator_id'],content=pm_msg)
     if live:
+      processed_modlogs['removed_posts'].extend(processed)
       if c not in available_communities['removed_posts']: # this is a new community, we need to add it so actions happen for new logs
         print(f'community with id {c} not seen previously, adding')
         available_communities['removed_posts'].append(c)
