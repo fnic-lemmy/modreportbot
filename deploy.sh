7ebaa6ee27a15e9ef4629fb4b8c53ed8b068110c
Chris Young <chris@unsatisfactorysoftware.co.uk>
2025-02-11 15:05:29 +0000

    Add Lemmy 1.0 test account

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ gc_deploy "dbzer0" "lemmy.dbzer0.com"
 gc_deploy "lemm-ee" "lemm.ee"
 gc_deploy "lemmy-world" "lemmy.world"
 gc_deploy "shitjustworks" "sh.itjust.works"
+gc_deploy "test" "voyager.lemmy.ml"
 
 gcloud run jobs deploy modreportbot-directorybot --project=$project --region=$region --source . --set-env-vars=LEMMY_USER="directorybot",LEMMY_INSTANCE="lemmy.dbzer0.com",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="$matrix_room",FLAG_DOWNVOTES="FALSE",GET_MESSAGES="TRUE",GET_REPORTS="FALSE",NOTIFY_NEW="TRUE",GET_MODLOG="FALSE",MODLOG_PM="FALSE" --set-secrets="LEMMY_PW=directorybot:latest","MATRIX_PW=fnicreports:latest" &
 
