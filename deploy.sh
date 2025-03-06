295750721b5701add1935f7074ceaf37d83bc004
Chris Young <chris@unsatisfactorysoftware.co.uk>
2025-03-06 21:03:14 +0000

    remove lemmings.world bot only looks after fairies, so moving to own bot

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ gc_deploy() {
   gcloud run jobs deploy modreportbot-$1 --project=$project --region=$region --source . --set-env-vars=LEMMY_USER="fnicmodbot",LEMMY_INSTANCE="$2",MATRIX_USER="@fnic_reports:matrix.org",MATRIX_SERVER="matrix.org",MATRIX_ROOM="$matrix_room",FLAG_DOWNVOTES="$flag_dv",GET_MESSAGES="$flag_pm",GET_REPORTS="$flag_reports",NOTIFY_NEW="$notify_new",GET_MODLOG="$get_modlog",MODLOG_PM="$modlog_pm" --set-secrets="LEMMY_PW=fnicmodbot-$1:latest","MATRIX_PW=fnicreports:latest" &
 }
 
-gc_deploy "lemmings-world" "lemmings.world"
 gc_deploy "dbzer0" "lemmy.dbzer0.com"
 gc_deploy "lemmy-world" "lemmy.world"
 gc_deploy "test" "voyager.lemmy.ml"
